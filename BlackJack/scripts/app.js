4bbd7d1a8ac7d4e5faa0c143b75e33b4880350ce
Hamid <118181996+Hamid3DATA@users.noreply.github.com>
2023-03-29 14:08:52 +0200

    Add files via upload

--- BlackJack/scripts/app.js
+++ BlackJack/scripts/app.js
@@ -189,9 +189,6 @@ function start() {
 
 function end() {
     if (end_count < 1) {
-
-        console.log("dealer aces: " + dealer_hand_aces)
-        console.log("your aces: " + your_hand_aces)
         
         end_count +=1
         start_count = 0
@@ -256,13 +253,7 @@ function end() {
             draw()
         }
 
-        console.log(your_hand)
-        console.log(dealer_hand)
-
         Cards = [["2", "Spade", 2], ["3", "Spade", 3], ["4", "Spade", 4], ["5", "Spade", 5], ["6", "Spade", 6], ["7", "Spade", 7], ["8", "Spade", 8], ["9", "Spade", 9], ["10", "Spade", 10], ["Jack", "Spade", 10], ["Queen", "Spade", 10], ["King", "Spade", 10], ["Ace", "Spade", 11], ["2", "Heart", 2], ["3", "Heart", 3], ["4", "Heart", 4], ["5", "Heart", 5], ["6", "Heart", 6], ["7", "Heart", 7], ["8", "Heart", 8], ["9", "Heart", 9], ["10", "Heart", 10], ["Jack", "Heart", 10], ["Queen", "Heart", 10], ["King", "Heart", 10], ["Ace", "Heart", 11], ["2", "Club", 2], ["3", "Club", 3], ["4", "Club", 4], ["5", "Club", 5], ["6", "Club", 6], ["7", "Club", 7], ["8", "Club", 8], ["9", "Club", 9], ["10", "Club", 10], ["Jack", "Club", 10], ["Queen", "Club", 10], ["King", "Club", 10], ["Ace", "Club", 11], ["2", "Diamond", 2], ["3", "Diamond", 3], ["4", "Diamond", 4], ["5", "Diamond", 5], ["6", "Diamond", 6], ["7", "Diamond", 7], ["8", "Diamond", 8], ["9", "Diamond", 9], ["10", "Diamond", 10], ["Jack", "Diamond", 10], ["Queen", "Diamond", 10], ["King", "Diamond", 10], ["Ace", "Diamond", 11]]
-
-        console.log(your_points)
-        console.log(dealer_points)
 
         your_hand_aces = 0
         dealer_hand_aces = 0
